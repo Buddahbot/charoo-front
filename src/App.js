baad649a6f37965cb56d1ff495aac8d8ee422ce8
Buddahbot <90890525+Buddahbot@users.noreply.github.com>
2022-02-15 00:15:23 +0100

    testchange

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Charoo <code></code>
+          Charoo <code></code> is here
         </p>
         <a
           className="App-link"
