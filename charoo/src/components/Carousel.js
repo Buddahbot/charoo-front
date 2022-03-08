d7cdf7d10203fd5cf50abe0826f0a2718d60b2a0
jcwalina <joannacwalinaa@gmail.com>
2022-03-08 15:43:35 +0100

    carousel

--- charoo/src/components/Carousel.js
+++ charoo/src/components/Carousel.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import ItemsCarousel from 'react-items-carousel';
 import '../stylesheets/Carousel.css'
 import { NavLink } from 'react-router-dom'
@@ -10,10 +10,17 @@ import BG4 from '../Img/help-sport.jpg'
 import BG5 from '../Img/run2.jpg'
 import BG6 from '../Img/run0.jpg'
 
+import { DonateContext } from '../context/DonateContext';
+import { useNavigate, Link } from 'react-router-dom';
 
-export default () => {
+const Carousel = () => {
+
+    const [data, setData] = useContext(DonateContext)
     const [activeItemIndex, setActiveItemIndex] = useState(0);
     const chevronWidth = 80;
+
+    let navigate = useNavigate()
+
     return (
         <div className='carousel' style={{ padding: `0 ${chevronWidth}px` }}>
             <ItemsCarousel
@@ -28,65 +35,39 @@ export default () => {
                 alwaysShowChevrons
             >
 
-                <NavLink style={{ textDecoration: 'none' }} to="/">
-                    <div className='carousele-item carousele-margin ' style={{ backgroundImage: `url(${BG1})` }}>
+                <NavLink style={{ textDecoration: 'none' }} to={`/donate2/${data.data[0]._id}/`} >
+                    <div className='carousele-item carousele-margin ' style={{ backgroundImage: `url(${data.data[0].imageUrl})` }}>
                         <div className='carousel-text'>
-                            <h1 className=''>ZYX card</h1>
+                            <h1 className=''>{data.data[0].eventTitle}</h1>
                         </div> </div> </NavLink>
 
-                <NavLink style={{ textDecoration: 'none' }} to="/">  <div className='carousele-item carousele-margin' style={{ backgroundImage: `url(${BG2})` }}>
-                    <div className='carousel-text'> <h1 className=''>ZYX card</h1> </div>
+                <NavLink style={{ textDecoration: 'none' }} to={`/donate2/${data.data[6]._id}/`}>  <div className='carousele-item carousele-margin' style={{ backgroundImage: `url(${data.data[6].imageUrl}})` }}>
+                    <div className='carousel-text'> <h1 className=''>{data.data[6].eventTitle}</h1> </div>
                 </div></NavLink>
 
-                <NavLink style={{ textDecoration: 'none' }} to="/">  <div className='carousele-item carousele-margin ' style={{ backgroundImage: `url(${BG3})` }}>
-                    <div className='carousel-text'> <h1 className=''>ZYX card</h1> </div>
+                <NavLink style={{ textDecoration: 'none' }} to={`/donate2/${data.data[2]._id}/`}>  <div className='carousele-item carousele-margin ' style={{ backgroundImage: `url(${data.data[2].imageUrl})` }}>
+                    <div className='carousel-text'> <h1 className=''>{data.data[2].eventTitle}</h1> </div>
                 </div></NavLink>
 
-                <NavLink style={{ textDecoration: 'none' }} to="/">  <div className='carousele-item carousele-margin' style={{ backgroundImage: `url(${BG4})` }}>
-                    <div className='carousel-text'> <h1 className=''>ZYX card</h1> </div>
+                <NavLink style={{ textDecoration: 'none' }} to={`/donate2/${data.data[3]._id}/`}>  <div className='carousele-item carousele-margin' style={{ backgroundImage: `url(${data.data[3].imageUrl})` }}>
+                    <div className='carousel-text'> <h1 className=''>{data.data[3].eventTitle}</h1> </div>
                 </div></NavLink>
 
-                <NavLink style={{ textDecoration: 'none' }} to="/">  <div className='carousele-item carousele-margin' style={{ backgroundImage: `url(${BG5})` }}>
-                    <div className='carousel-text'> <h1 className=''>ZYX card</h1> </div>
+                <NavLink style={{ textDecoration: 'none' }} to={`/donate2/${data.data[4]._id}/`}>  <div className='carousele-item carousele-margin' style={{ backgroundImage: `url(${data.data[4].imageUrl})` }}>
+                    <div className='carousel-text'> <h1 className=''>{data.data[4].eventTitle}</h1> </div>
                 </div></NavLink>
 
-                <NavLink style={{ textDecoration: 'none' }} to="/">   <div className='carousele-item carousele-margin' style={{ backgroundImage: `url(${BG6})` }}>
-                    <div className='carousel-text'>  <h1 className=''>ZYX card</h1> </div>
+                <NavLink style={{ textDecoration: 'none' }} to={`/donate2/${data.data[5]._id}/`}>   <div className='carousele-item carousele-margin' style={{ backgroundImage: `url(${data.data[5].imageUrl})` }}>
+                    <div className='carousel-text'>  <h1 className=''>{data.data[5].eventTitle}</h1> </div>
                 </div></NavLink>
 
             </ItemsCarousel>
         </div>
-    );
+    )
+
+
+
+
 };
 
-
-// const Description = styled.div`
-//   display: none;
-//   text-align: center;
-//   padding-top: 2rem;
-//   color: rgba(23, 23, 23, 0.8);
-// `;
-
-// const RecipeInfo = styled.div`
-//   width: 100%;
-//   height: 40%;
-//   bottom: 40% !important;
-//   position: relative;
-//   background-color: rgba(238, 238, 238, 0.85);
-//   display: flex;
-//   flex-direction: column;
-//   justify-content: center;
-//   align-items: center;
-//   transition-duration: 0.5s;
-//   padding: 2rem 3rem;
-//   border-radius: 15px !important;
-
-//   &:hover {
-//     height: 60%;
-//     bottom: 60% !important;
-//   }
-
-//   &:hover ${Description} {
-//     display: inline;
-//   }
-// `;
+export default Carousel 
